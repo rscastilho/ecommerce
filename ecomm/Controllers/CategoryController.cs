c42c8d535b49b7b8a150e8db0851ca16d44fc6f1
rscastilho <rcastilho@gmail.com>
2025-04-11 20:22:12 -0300

    add_new_controllers

---
+++ ecomm/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecomm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
